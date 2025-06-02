fd824c01fb7d6c8becef384900fc8d912dbffe96
rajan <kumar.rajan@mapmyindia.com>
2025-06-02 12:23:54 +0530

    D_version_2

---
+++ docs/web/v1.0.1/android.md
@@ -0,0 +1,14 @@
+---
+id: mapping-web
+title: web Docs
+sidebar_label: Web Docs
+slug: /webDocs
+---
+# Mappls Map Android SDK
+
+**Easy To Integrate Maps & Location APIs & SDKs For Android Applications**
+
+
+1. You can get your api key to be used in this document here: [https://about.mappls.com/api/signup](https://about.mappls.com/api/signup)
+
+2. The sample code is provided to help you understand the basic functionality of Mappls maps & REST APIs working on **Android** native development platform.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,31 +1,20 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
-
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const github = require('prism-react-renderer/themes/github');
+const dracula = require('prism-react-renderer/themes/dracula');
+const path = require('path');
+const {themes: prismThemes} = require('prism-react-renderer');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
-
-
-  url: 'https://your-docusaurus-test-site.com',
-
+  url: 'https://developer-rajan.netlify.app/',
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'KumarRajan1',
+  projectName: 'ci integration',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -34,105 +23,89 @@ const config = {
   presets: [
     [
       'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
+          routeBasePath: '/', // URL prefix (change to '/' to use docs at root)
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
+      },
     ],
   ],
+  
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+  plugins: [
+    // [
+    //   '@docusaurus/plugin-content-docs',
+    //   {
+    //     id: 'mapping',
+    //     path: 'mapping',
+    //     routeBasePath: 'mapping',
+    //     sidebarPath: require.resolve('./sidebars.js'),
+    //     include: ['**/*.md'],
+    //   },
+    // ],
+    // [
+    //   '@docusaurus/plugin-content-docs',
+    //   {
+    //     id: 'analytics',
+    //     path: 'analytics',
+    //     routeBasePath: 'analytics',
+    //     sidebarPath: require.resolve('./sidebars.js'),
+    //   },
+    // ],
+    // [
+    //   '@docusaurus/plugin-content-docs',
+    //   {
+    //     id: 'telematics',
+    //     path: 'telematics',
+    //     routeBasePath: 'telematics',
+    //     sidebarPath: require.resolve('./sidebars.js'),
+    //   },
+    // ],
+  ],
+  customFields: {
+    buildTimestamp: Date.now(),
+  },
+
+  themeConfig: {
+     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    navbar: {
+      title: 'My Site',
+      logo: {
+        alt: 'Site Logo',
+        src: 'img/logo.svg',
+        href: '/web',
+      },
+      items: [
+        { to: 'mapping/web', label: 'Mapping', position: 'left' },
+          // { to: 'analytics/mgis-widgets', label: 'Analytics', position: 'left' },
+          // { to: 'telematics/get-your-key', label: 'Telematics', position: 'left' },
+        {
+          href: 'https://github.com/my-org/my-docusaurus-site',
+          label: 'GitHub',
+          position: 'right',
         },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            { label: 'Mapping', to: 'mapping/web' },
+            // { label: 'Analytics', to: 'analytics/mgis-widgets' },
+            // { label: 'Telematics', to: '/telematics' },
+          ],
+        },
+      ],
+      copyright: `© ${new Date().getFullYear()}`,
+    },
+    prism: {
+      theme: github,
+      darkTheme: dracula,
+    },
+  },
 };
 
 module.exports = config;
